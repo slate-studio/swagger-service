bf790f458eff5f52751e5244548d1c82470d2b58
Artem <artem@slatestudio.com>
2017-06-20 14:11:59 +0300

    Remove corsOptions

--- index.js
+++ index.js
@@ -56,19 +56,11 @@ const expressHealth = (express) => {
   const health = require('./lib/express/health')
   const cors   = require('cors')
 
-  var corsOptions = {
-    origin: function (origin, callback) {
+  if (process.env.NODE_ENV != 'production')
+    express.get(`${_basePath}/health`, cors(), health)
+  else
+    express.get(`${_basePath}/health`, health)
 
-      if (process.env.NODE_ENV != 'production1') {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-
-    }
-  }
-
-  express.get(`${_basePath}/health`, cors(corsOptions), health)
 }
 
 const expressDocumentation = (express) => {
