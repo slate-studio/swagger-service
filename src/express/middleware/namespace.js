340e718f906d8a088c707c634bcc676a42fd6808
Sergey Oprya <sergey@slatestudio.com>
2017-09-20 13:14:42 +0300

    Fix

--- src/express/middleware/namespace.js
+++ src/express/middleware/namespace.js
@@ -4,5 +4,5 @@ const RequestNamespace = require('../../utils/requestNamespace')
 
 module.exports = (req, res, next) => {
   req.requestNamespace = new RequestNamespace(req.headers)
-  req.requestNamespace.save({req, res}, next)
+  req.requestNamespace.save([ req, res ], next)
 }
