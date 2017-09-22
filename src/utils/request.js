a08f137cd1c01b53387edfe561208a8a520cb756
Alexander Kravets <a@kra.vc>
2017-09-22 17:18:28 +0300

    Add support for HTTPS for request utility

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,14 @@
 'use strict'
 
-const http = require('http')
+const http  = require('http')
+const https = require('https')
 
 module.exports = (options) => {
+  const scheme = _.get(options, 'scheme', 'http')
+  const client = scheme === 'https' ? https : http
+
   return new Promise((resolve, reject) => {
-    const req = http.request(options, (res) => {
+    const req = client.request(options, (res) => {
       res.on('data', chunk => res.body = (res.body || '') + chunk)
 
       res.on('end', () => {
@@ -16,7 +20,9 @@ module.exports = (options) => {
     req.on('error', reject)
 
     if (options.body) {
-      req.write(options.body)
+      const json   = JSON.stringify(options.body)
+      const buffer = new Buffer(json)
+      req.write(buffer)
     }
 
     req.end()
