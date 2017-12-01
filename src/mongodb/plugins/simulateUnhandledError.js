38e4f444773f2e95e7e7b0635ae471925983c7b0
Zablotskiy <serhiyzablotskiyweb@gmail.com>
2017-12-01 11:50:58 +0200

    Add skip parameter to simulate unhandled error helper

--- src/mongodb/plugins/simulateUnhandledError.js
+++ src/mongodb/plugins/simulateUnhandledError.js
@@ -1,38 +1,49 @@
 'use strict'
 
-const MONGOOSE_HOOKS = [ 'save',
-                         'update',
-                         'remove',
-                         'count',
-                         'find',
-                         'findOne',
-                         'findOneAndRemove',
-                         'findOneAndUpdate',
-                         'insertMany' ]
-
-const checkUnhandledErrorFlag = (next, schema) => {
-  if (schema.simulateUnhandledErrorFlag) {
-    schema.simulateUnhandledErrorFlag = false
-
-    const err = new Error('SIMULATED_MONGOOSE_UNHANDLED_ERROR')
-    next(err)
-
-  } else {
-    next()
-
+class UnhandledMongodbError extends Error {
+  constructor() {
+    super('Simulated unhandled database query error')
+    this.name = this.constructor.name
   }
 }
 
-module.exports = (schema) => {
+const MONGOOSE_HOOKS = [
+  'save',
+  'update',
+  'remove',
+  'count',
+  'find',
+  'findOne',
+  'findOneAndRemove',
+  'findOneAndUpdate',
+  'insertMany' ]
+
+const checkUnhandledErrorFlag = (next, schema) => {
+  if (schema.simulateUnhandledErrorFlag) {
+    if (schema.simulateUnhandledErrorSkip == 0) {
+      schema.simulateUnhandledErrorFlag = false
+
+      const error = new UnhandledMongodbError()
+      return next(error)
+    }
+
+    schema.simulateUnhandledErrorSkip -= 1
+  }
+
+  return next()
+}
+
+module.exports = schema => {
   schema.simulateUnhandledErrorFlag = false
 
-  MONGOOSE_HOOKS.forEach((hook) => {
+  MONGOOSE_HOOKS.forEach(hook => {
     schema.pre(hook, function(next) {
       checkUnhandledErrorFlag(next, schema)
     })
   })
 
-  schema.simulateUnhandledError = () => {
+  schema.simulateUnhandledError = (skip = 0) => {
     schema.simulateUnhandledErrorFlag = true
+    schema.simulateUnhandledErrorSkip = skip
   }
 }
