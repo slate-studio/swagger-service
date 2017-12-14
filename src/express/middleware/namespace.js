545f5dd349170cfc46385818b625f4e503b8d607
Sergey Oprya <sergey@slatestudio.com>
2017-12-14 18:06:39 +0200

    Add x-request-id header to response

--- src/express/middleware/namespace.js
+++ src/express/middleware/namespace.js
@@ -3,6 +3,7 @@
 const RequestNamespace = require('../../utils/requestNamespace')
 
 module.exports = (req, res, next) => {
+  res.setHeader('x-request-id', req.headers['x-request-id'])
   req.requestNamespace = new RequestNamespace(req.headers)
   req.requestNamespace.save([ req, res ], next)
 }
