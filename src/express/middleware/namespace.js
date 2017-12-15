89e922483d8141620a485a219a6154438a8c5eb8
Sergey Oprya <sergey@slatestudio.com>
2017-12-15 11:56:45 +0200

    Fix

--- src/express/middleware/namespace.js
+++ src/express/middleware/namespace.js
@@ -3,7 +3,11 @@
 const RequestNamespace = require('../../utils/requestNamespace')
 
 module.exports = (req, res, next) => {
-  res.setHeader('x-request-id', req.headers['x-request-id'])
+  const requestId = req.headers['x-request-id']
+  if (requestId) {
+    res.setHeader('x-request-id', requestId)
+  }
+
   req.requestNamespace = new RequestNamespace(req.headers)
   req.requestNamespace.save([ req, res ], next)
 }
