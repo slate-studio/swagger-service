6b46ae865551c520583b402828dd1cf18c16bd7c
seeruyy <seeruyy@gmail.com>
2017-12-15 11:58:30 +0200

    Add request id to response (#33)
    * Add x-request-id header to response

--- src/express/middleware/namespace.js
+++ src/express/middleware/namespace.js
@@ -3,6 +3,11 @@
 const RequestNamespace = require('../../utils/requestNamespace')
 
 module.exports = (req, res, next) => {
+  const requestId = req.headers['x-request-id']
+  if (requestId) {
+    res.setHeader('x-request-id', requestId)
+  }
+
   req.requestNamespace = new RequestNamespace(req.headers)
   req.requestNamespace.save([ req, res ], next)
 }
